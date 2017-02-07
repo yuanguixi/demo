e6ed7ef754ad16ec50f3e98ddeee1779118ed982
YuanGuixi <cy_ygx@126.com>
2017-02-07 15:08:03 +0800

    添加第三方 库   Airbnb 动画库Lottie

--- app/src/main/java/com/yguixi/demo_yguixi/MainActivity.java
+++ app/src/main/java/com/yguixi/demo_yguixi/MainActivity.java
@@ -1,13 +1,34 @@
 package com.yguixi.demo_yguixi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.yguixi.demo_yguixi.ui.FirstActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+    }
+
+    public void initUI() {
+        Button btn = (Button) findViewById(R.id.animal);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.animal:
+                Intent intent = new Intent();
+                intent.setClass(this, FirstActivity.class);
+                startActivity(intent);
+        }
     }
 }
